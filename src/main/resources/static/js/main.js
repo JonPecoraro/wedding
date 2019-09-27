ca1b4ec859c6278a11e5661e93ebae4704f8b350
Jonathan Pecoraro <jonpecoraro@gmail.com>
2019-09-27 13:22:27 -0400

    Fixes mobile view

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,24 +1,34 @@
 function scrollToAnchor(url) {
 	var splitUrl = url.split('/');
 	var anchorId = splitUrl[splitUrl.length - 1];
-	var $contentHider = $('#content-hider');
-	var offsetFromTop = $contentHider.is(':visible') ? $contentHider[0].clientHeight : 0;
+	var offsetFromTop = $('#nav')[0].clientHeight + 30;
+	
+	// Make sure the mobile menu is collapsed
+	$('#navbarSupportedContent').collapse('hide');
 	
 	$('html,body').animate({
 		scrollTop: $(anchorId).offset().top - offsetFromTop
     }, 'slow');
+	
+	$('.dropdown-item').removeClass('active');
+	$('.dropdown-item[href="' + url + '"]').addClass('active');
 }
 
 function handleNavBarHittingTop() {
-	var $nav = $('#nav');
-	var navBarTopPosition = 200;
-	if ($nav.offset().top >= navBarTopPosition) {
-		if ($nav.hasClass('bg-transparent')) {
-			$nav.removeClass('bg-transparent');
-		}
-	} else {
-		if (!$nav.hasClass('bg-transparent')) {
-			$nav.addClass('bg-transparent');
+	var isMobileView = $('body').css('background-image') == 'none';
+	if (!isMobileView) {
+		var $nav = $('#nav');
+		var navBarTopPosition = 200;
+		if ($nav.offset().top >= navBarTopPosition) {
+			if ($nav.hasClass('bg-transparent')) {
+				$nav.removeClass('bg-transparent');
+				$nav.css('box-shadow', '0px 10px 20px 0px #FCFCFC');
+			}
+		} else {
+			if (!$nav.hasClass('bg-transparent')) {
+				$nav.addClass('bg-transparent');
+				$nav.css('box-shadow', 'none');
+			}
 		}
 	}
 }
@@ -36,6 +46,6 @@ $(function() {
 	$(window).scroll(handleNavBarHittingTop);
 	
 	if (window.location.href.indexOf('#') > 0) {
-		scrollToAnchor(window.location.href);
+		scrollToAnchor(window.location.pathname + window.location.hash);
 	}
 })
