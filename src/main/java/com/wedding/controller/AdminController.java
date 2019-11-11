b4ab4f53dfbfa82fa748c7a5df99a9e360149a6f
Jonathan Pecoraro <jonpecoraro@gmail.com>
2019-11-11 07:59:21 -0500

    Added admin tools and fixed typos

--- src/main/java/com/wedding/controller/AdminController.java
+++ src/main/java/com/wedding/controller/AdminController.java
@@ -3,11 +3,18 @@ package com.wedding.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wedding.dao.GuestRepository;
 import com.wedding.dao.GuestbookRepository;
+import com.wedding.model.Guest;
+import com.wedding.model.Guestbook;
 
 @Controller
 @RequestMapping(value = "/admin", produces = "application/json")
@@ -30,5 +37,52 @@ public class AdminController {
 	public String showGuestbookEntries(Model model) {
 		model.addAttribute("guestbook", guestbookRepository.findAll());
 		return "admin/guestbook";
+	}
+	
+	@PostMapping("/edit-guest/{id}")
+	public String editGuest(String firstName, String lastName, String suffix, String email, String rsvpResponse, String foodChoice, String plansToUseRoomBlock, @PathVariable int id) {
+		guestRepository.findById(id)
+			.map(guest -> {
+				guest.setEmail(email);
+				guest.setFirstName(firstName);
+				guest.setLastName(lastName);
+				guest.setFoodChoice(foodChoice);
+				guest.setPlansToUseRoomBlock(plansToUseRoomBlock);
+				guest.setRsvpResponse(rsvpResponse);
+				guest.setSuffix(suffix);
+				return guestRepository.save(guest);
+			})
+			.orElseGet(() -> {
+				return null;
+			});
+		
+		return "redirect:/admin/guest-list";
+	}
+	
+	@PostMapping("/edit-guestbook-entry/{id}")
+	public String editGuestbookEntry(String name, String message, @PathVariable int id) {
+		guestbookRepository.findById(id)
+			.map(entry -> {
+				entry.setMessage(message);
+				entry.setName(name);
+				return guestbookRepository.save(entry);
+			})
+			.orElseGet(() -> {
+				return null;
+			});
+		
+		return "redirect:/admin/guestbook";
+	}
+	
+	@PostMapping("/delete-guest/{id}")
+	public String deleteGuest(@PathVariable int id) {
+		guestRepository.deleteById(id);
+		return "redirect:/admin/guest-list";
+	}
+	
+	@PostMapping("/delete-guestbook-entry/{id}")
+	public String deleteGuestbookEntry(@PathVariable int id) {
+		guestbookRepository.deleteById(id);
+		return "redirect:/admin/guestbook";
 	}
 }
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,3 +1,60 @@
 $(function() {
 	$('.display').DataTable();
+	
+	// Guest admin page functions	
+	$('.guest-link').on('click', function() {
+		var $this = $(this);
+		$('#updateGuestForm').attr('action', '/admin/edit-guest/' + $this.attr('id'));
+		$('#guestId').val($this.attr('id'));
+		$('#firstName').val($this.attr('first-name'));
+		$('#lastName').val($this.attr('last-name'));
+		$('#suffix').val($this.attr('suffix'));
+		$('#email').val($this.attr('email'));
+		$('#rsvpResponse').val($this.attr('rsvp-response'));
+		$('#foodChoice').val($this.attr('food-choice'));
+		$('#plansToUseRoomBlock').val($this.attr('plans-to-use-room-block'));
+		$('#editGuestModal').modal();
+	});
+	
+	$('#updateGuestBtn').on('click', function() {
+		$('#updateGuestForm').submit();
+	});
+	
+	$('#deleteGuestBtn').on('click', function() {
+		var deleteConfirmation = confirm("Are you sure you want to delete this guest?");
+		if (deleteConfirmation == true) {
+			$('#updateGuestForm')
+				.attr('action', '/admin/delete-guest/' + $('#guestId').val())
+				.submit();
+		} else {
+			$('#editGuestModal').modal('hide');
+		}
+	});
+	// End Guest admin page functions
+	
+	// Guestbook admin page function
+	$('.guestbook-entry-link').on('click', function() {
+		var $this = $(this);
+		$('#updateEntryForm').attr('action', '/admin/edit-guestbook-entry/' + $this.attr('id'));
+		$('#entryId').val($this.attr('id'));
+		$('#name').val($this.attr('name'));
+		$('#message').val($this.attr('message'));
+		$('#ediEntrytModal').modal();
+	});
+	
+	$('#updateEntryBtn').on('click', function() {
+		$('#updateEntryForm').submit();
+	});
+	
+	$('#deleteEntryBtn').on('click', function() {
+		var deleteConfirmation = confirm("Are you sure you want to delete this guestbook entry?");
+		if (deleteConfirmation == true) {
+			$('#updateEntryForm')
+				.attr('action', '/admin/delete-guestbook-entry/' + $('#entryId').val())
+				.submit();
+		} else {
+			$('#ediEntrytModal').modal('hide');
+		}
+	});
+	// End Guestbook admin page function
 });
